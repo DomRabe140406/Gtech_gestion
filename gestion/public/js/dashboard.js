155a42cf68392bf32418731e279e09e5de31decd
bakeneko_sb <clawdeenwolfmendrika@gmail.com>
2026-07-31 12:25:17 +0100

    Résolution conflit dashboard

--- gestion/public/js/dashboard.js
+++ gestion/public/js/dashboard.js
@@ -1,3 +1,276 @@
-   function Menu() {
+function Menu() {
   document.getElementById("sidebar").classList.toggle("active");
 }
+
+//pour le Doughnut Chart
+const ctx = document.getElementById('formationsChart');
+
+const centerTextPlugin = {
+  id: 'centerText',
+
+  beforeDraw(chart) {
+    const {
+            width,
+            height,
+            ctx
+          } = chart;
+
+    ctx.restore();
+
+    ctx.font = 'bold 28px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    ctx.fillText(
+        total,
+          width / 2,
+          height / 2 - 30
+    );
+
+    ctx.font = '14px Arial';
+
+    ctx.fillText(
+        'Formations',
+          width / 2,
+          height / 2
+    );
+
+    ctx.save();
+  }
+};
+
+console.log('window.Chart = ', window.Chart);
+const { enInscription, enCours, termine, total } = window.dashboardData;
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+          labels: [
+                    'En inscription',
+                    'En cours',
+                    'Terminées'
+                ],
+
+          datasets: [{
+                    data: [
+                        enInscription,
+                        enCours,
+                        termine
+                    ] ,
+
+                    backgroundColor: [
+                                  '#3B82F6',
+                                  '#F59E0B',
+                                  '#10B981'
+                              ],
+
+                    borderWidth: 2
+          }]
+  },
+
+  options: {
+
+            responsive: true,
+
+            cutout: '70%',
+
+            plugins: {
+
+                    legend: {
+                        position: 'bottom'
+                    },
+
+                    tooltip: {
+                        callbacks: {
+                            label: function(context) {
+
+                                let totalFormation = total;
+                                let value = context.raw;
+
+                                let percentage =
+                                    ((value / totalFormation) * 100)
+                                    .toFixed(1);
+
+                                return context.label +
+                                    ': ' +
+                                    value +
+                                    ' (' +
+                                    percentage +
+                                    '%)';
+                            }
+                        }
+                      }
+            }
+  },
+
+  plugins: [centerTextPlugin]
+});
+
+//pour le graphe
+const allLabels = window.formationData.labels;
+const allData = window.formationData.data;
+
+// Nombre de mois affichés
+const windowSize = 3;
+
+// Position de départ
+let startIndex = 0;
+const ctx2 = document.getElementById('formationGraphe');
+
+const chart = new Chart(ctx2, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{
+                    label: 'Formations créées',
+                    data: [],
+                    borderColor: '#2563eb',
+                    backgroundColor: 'rgba(37,99,235,.2)',
+                    borderWidth: 3,
+                    fill: true,
+                    tension: 0.4,
+                    pointRadius: 5,
+                    pointHoverRadius: 8
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: true
+                    }
+                },
+
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            stepSize: 1,
+                            precision:0
+                        }
+                    }
+                }
+            }
+});
+
+//elle découpe le tableau
+function updateChart(){
+
+  chart.data.labels = allLabels.slice(
+          startIndex,
+          startIndex + windowSize
+  );
+
+  chart.data.datasets[0].data = allData.slice(
+            startIndex,
+            startIndex + windowSize
+  );
+  chart.update();
+}
+
+//elle regarde si on peut aller à gauche ou à droite (actuellement on ne peut pas aller avant janv et pas après dec)
+function updateButtons() {
+  const btnPrev = document.getElementById('btnPrev');
+  const btnNext = document.getElementById('btnNext');
+
+  // Bouton précédent
+  btnPrev.disabled = (startIndex === 0);
+
+  // Bouton suivant
+  btnNext.disabled = (startIndex >= allLabels.length - windowSize);
+        
+  // Apparence du bouton précédent
+  if (btnPrev.disabled) {
+
+    btnPrev.classList.remove(
+                    'bg-blue-500',
+                    'text-white',
+                    'hover:bg-blue-600',
+                    'cursor-pointer'
+    );
+
+    btnPrev.classList.add(
+                    'bg-gray-300',
+                    'text-gray-500',
+                    'cursor-not-allowed',
+                    'opacity-50'
+    );
+
+  } else {
+
+    btnPrev.classList.remove(
+                    'bg-gray-300',
+                    'text-gray-500',
+                    'cursor-not-allowed',
+                    'opacity-50'
+    );
+
+    btnPrev.classList.add(
+                    'bg-blue-500',
+                    'text-white',
+                    'hover:bg-blue-600',
+                    'cursor-pointer'
+    );
+
+  }
+
+  // Apparence du bouton suivant
+  if (btnNext.disabled) {
+
+    btnNext.classList.remove(
+                    'bg-blue-500',
+                    'text-white',
+                    'hover:bg-blue-600',
+                    'cursor-pointer'
+    );
+
+    btnNext.classList.add(
+                    'bg-gray-300',
+                    'text-gray-500',
+                    'cursor-not-allowed',
+                    'opacity-50'
+    );
+  } else {
+
+    btnNext.classList.remove(
+                    'bg-gray-300',
+                    'text-gray-500',
+                    'cursor-not-allowed',
+                    'opacity-50'
+    );
+
+    btnNext.classList.add(
+                    'bg-blue-500',
+                    'text-white',
+                    'hover:bg-blue-600',
+                    'cursor-pointer'
+    );
+
+  }
+}
+
+//flèche Next
+document
+      .getElementById('btnNext')
+      .addEventListener('click', function () {
+        if (startIndex < allLabels.length - windowSize) {
+          startIndex++;
+          updateChart();
+          updateButtons();
+        }
+});
+
+//flèche Prev
+document
+      .getElementById('btnPrev')
+      .addEventListener('click', function () {
+        if (startIndex > 0) {
+          startIndex--;
+          updateChart();
+          updateButtons();
+        }
+});
+
+
+updateChart();
+updateButtons();
--- gestion/resources/views/dashboard.blade.php
+++ gestion/resources/views/dashboard.blade.php
@@ -157,9 +157,7 @@
                 </h2>
 
                 <div class="h-72 flex justify-center items-center">
-
                     <canvas id="formationsChart"></canvas>
-
                 </div>
 
         </div>
@@ -295,386 +293,53 @@
 
             <img src="{{ asset('img/Logo.png') }}" class="h-10" alt="Logo">
 
-            <div>
-
-                <h3 class="font-bold text-gray-700 dark:text-gray-100">
-                    GASY TECH
-                </h3>
-
-                <p class="text-sm text-gray-500 dark:text-gray-100">
-                    Gestion intelligente des formations
-                </p>
-
-            </div>
-
         </div>
 
         <div class="text-center">
-
             <p class="text-gray-500 text-sm dark:text-gray-100">
-
                 © {{ date('Y') }}
                 <span class="font-semibold text-gray-700 dark:text-gray-100">
                     GASY TECH
                 </span>
-
                 • Tous droits réservés.
-
             </p>
-
+            <p class=" italic text-gray-500 dark:text-gray-100">"Ny Fahaizana no ampinga enti-miady"</p>
         </div>
 
         <!-- icones reseaux sociaux -->
-
         <div class="flex gap-4">
-
             <a href="#" class="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-blue-500 hover:text-white transition">
-
                 <i class="fab fa-facebook-f"></i>
-
             </a>
-
             <a href="#" class="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-sky-500 hover:text-white transition">
-
                 <i class="fab fa-twitter"></i>
-
             </a>
-
             <a href="#" class="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-blue-700 hover:text-white transition">
-
                 <i class="fab fa-linkedin-in"></i>
-
             </a>
-
             <a href="#" class="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center text-gray-600 hover:bg-red-500 hover:text-white transition">
-
                 <i class="fas fa-envelope"></i>
-
             </a>
-
         </div>
-
     </div>
-
 </footer>
-<!--verification 
-<pre>
-{{ json_encode($labels, JSON_PRETTY_PRINT) }}
-</pre>
 
-<pre>
-{{ json_encode($data, JSON_PRETTY_PRINT) }}
-</pre>-->
 @endsection
 @section('scripts')
-    <!--pour le Doughnut Chart -->
-    <script>
-        const ctx = document.getElementById('formationsChart');
-
-        const centerTextPlugin = {
-            id: 'centerText',
-
-            beforeDraw(chart) {
-                const {
-                    width,
-                    height,
-                    ctx
-                } = chart;
-
-                ctx.restore();
-
-                ctx.font = 'bold 28px Arial';
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'middle';
-
-                ctx.fillText(
-                    '{{ $total }}',
-                    width / 2,
-                    height / 2 - 30
-                );
-
-                ctx.font = '14px Arial';
-
-                ctx.fillText(
-                    'Formations',
-                    width / 2,
-                    height / 2
-                );
-
-                ctx.save();
-            }
-        };
-
-        console.log('window.Chart = ', window.Chart);
-        new Chart(ctx, {
-
-            type: 'doughnut',
-
-            data: {
-                labels: [
-                    'En inscription',
-                    'En cours',
-                    'Terminées'
-                ],
-
-                datasets: [{
-                    data: [
-                        {{ $enInscription }},
-                        {{ $enCours }},
-                        {{ $termine }}
-                    ],
-
-                    backgroundColor: [
-                        '#3B82F6',
-                        '#F59E0B',
-                        '#10B981'
-                    ],
-
-                    borderWidth: 2
-                }]
-            },
-
-            options: {
-
-                responsive: true,
-
-                cutout: '70%',
-
-                plugins: {
-
-                    legend: {
-                        position: 'bottom'
-                    },
-
-                    tooltip: {
-                        callbacks: {
-                            label: function(context) {
-
-                                let total = {{ $total }};
-                                let value = context.raw;
-
-                                let percentage =
-                                    ((value / total) * 100)
-                                    .toFixed(1);
-
-                                return context.label +
-                                    ': ' +
-                                    value +
-                                    ' (' +
-                                    percentage +
-                                    '%)';
-                            }
-                        }
-                    }
-                }
-            },
-
-            plugins: [centerTextPlugin]
-        });
-    </script>
-    
-    <!-- pour le graphe -->
-    <script>
-        const allLabels = @json($labels);
-        const allData = @json($data);
-
-        // Nombre de mois affichés
-        const windowSize = 3;
-
-        // Position de départ
-        let startIndex = 0;
-        /*const labels = @json($labels);
-        const data = @json($data);*/
-        const ctx2 = document.getElementById('formationGraphe');
-        //console.log('window.Chart = ', window.Chart);
-        const chart = new Chart(ctx2, {
-
-            type: 'line',
-
-            data: {
-
-                labels: [],
-
-                datasets: [{
-
-                    label: 'Formations créées',
-
-                    data: [],
-
-                    borderColor: '#2563eb',
-
-                    backgroundColor: 'rgba(37,99,235,.2)',
-
-                    borderWidth: 3,
-
-                    fill: true,
-
-                    tension: 0.4,
-
-                    pointRadius: 5,
-
-                    pointHoverRadius: 8
-
-                }]
-
-            },
-
-            options: {
-
-                responsive: true,
-                maintainAspectRatio: false,
-
-                plugins: {
-
-                    legend: {
-
-                        display: true
-
-                    }
-
-                },
-
-                scales: {
-                   
-                    y: {
-                        beginAtZero: true,
-                        
-                        ticks: {
-                            stepSize: 1,
-                            precision:0
-                        }
-                    }
-                }
-
-            }
-
-        });
-
-        //elle découpe le tableau
-        function updateChart(){
-
-            chart.data.labels = allLabels.slice(
-                startIndex,
-                startIndex + windowSize
-            );
-
-            chart.data.datasets[0].data = allData.slice(
-                startIndex,
-                startIndex + windowSize
-            );
-
-            chart.update();
-
-        }
-
-        //elle regarde si on peut aller à gauche ou à droite (actuellement on ne peut pas aller avant janv et pas après dec)
-        function updateButtons() {
-            const btnPrev = document.getElementById('btnPrev');
-            const btnNext = document.getElementById('btnNext');
-
-            // Bouton précédent
-            btnPrev.disabled = (startIndex === 0);
-
-            // Bouton suivant
-            btnNext.disabled = (startIndex >= allLabels.length - windowSize);
-        
-            // Apparence du bouton précédent
-            if (btnPrev.disabled) {
-
-                btnPrev.classList.remove(
-                    'bg-blue-500',
-                    'text-white',
-                    'hover:bg-blue-600',
-                    'cursor-pointer'
-                );
-
-                btnPrev.classList.add(
-                    'bg-gray-300',
-                    'text-gray-500',
-                    'cursor-not-allowed',
-                    'opacity-50'
-                );
-
-            } else {
-
-                btnPrev.classList.remove(
-                    'bg-gray-300',
-                    'text-gray-500',
-                    'cursor-not-allowed',
-                    'opacity-50'
-                );
-
-                btnPrev.classList.add(
-                    'bg-blue-500',
-                    'text-white',
-                    'hover:bg-blue-600',
-                    'cursor-pointer'
-                );
-
-            }
-
-            // Apparence du bouton suivant
-            if (btnNext.disabled) {
-
-                btnNext.classList.remove(
-                    'bg-blue-500',
-                    'text-white',
-                    'hover:bg-blue-600',
-                    'cursor-pointer'
-                );
-
-                btnNext.classList.add(
-                    'bg-gray-300',
-                    'text-gray-500',
-                    'cursor-not-allowed',
-                    'opacity-50'
-                );
-
-            } else {
-
-                btnNext.classList.remove(
-                    'bg-gray-300',
-                    'text-gray-500',
-                    'cursor-not-allowed',
-                    'opacity-50'
-                );
-
-                btnNext.classList.add(
-                    'bg-blue-500',
-                    'text-white',
-                    'hover:bg-blue-600',
-                    'cursor-pointer'
-                );
-
-            }
-        }
-
-        //flèche Next
-        document
-            .getElementById('btnNext')
-            .addEventListener('click', function () {
-                if (startIndex < allLabels.length - windowSize) {
-                    startIndex++;
-                    updateChart();
-                    updateButtons();
-                }
-            });
-
-        //flèche Prev
-        document
-            .getElementById('btnPrev')
-            .addEventListener('click', function () {
-                if (startIndex > 0) {
-                    startIndex--;
-                    updateChart();
-                    updateButtons();
-                }
-            });
-
-
-        updateChart();
-        updateButtons();
-
-    </script>
+<script>
+    //pour le doughnut
+    window.dashboardData = {
+        enInscription: @json($enInscription),
+        enCours: @json($enCours),
+        termine: @json($termine),
+        total: @json($total)
+    };
+    //pour le graphe
+    window.formationData = {
+        labels: @json($labels),
+        data: @json($data)
+    };
+</script>
+@vite('public/js/dashboard.js')
 
 @endsection
