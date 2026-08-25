3eb1442f5cf5cbd01c8f0bc94b044d4519cbc9ff
DomRabe140406 <domoina.rabetrano@esti.mg>
2026-08-25 20:06:23 +0300

    correction

--- gestion/resources/views/layouts/auth.blade.php
+++ gestion/resources/views/layouts/auth.blade.php
@@ -1,40 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 <head>
-
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/notification.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <title>@yield('title')</title>
-
-    <style>
-        html {
-            visibility: hidden;
-        }
-
-        html.css-ready {
-            visibility: visible;
-        }
-    </style>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-
-    <link rel="stylesheet"
-          href="{{ asset('css/notification.css') }}">
-
 </head>
-
 <body>
-
+    
     @yield('content')
-
-    <script>
-        document.documentElement.classList.add('css-ready');
-    </script>
 
 </body>
 </html>
